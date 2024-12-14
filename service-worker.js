a2be18b4991bf75647e3a78bc5d59ea3bb19678e
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2024-12-14 09:52:56 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,42 @@
-const CACHE_NAME = 'oau-docs-cache';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './pdf.worker.js',
+const CACHE_NAME = 'oau-docs-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/libs/pdf.js',
+  '/libs/pdf.worker.js',
+  '/libs/mammoth.browser.min.js',
+  '/libs/pptxgen.min.js'
 ];
 
+// Install Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+      console.log('Caching assets');
+      return cache.addAll(ASSETS);
     })
   );
 });
 
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Cached Files
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
